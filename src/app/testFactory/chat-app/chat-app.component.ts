8d31429de08abe16573f87d153f8dfb466c4a3a6
Houdhey <houdheyfa.bouzid@intech.lu>
2022-11-06 19:16:07 +0100

    Add Quiz Page

---
+++ src/app/testFactory/chat-app/chat-app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-app',
+  templateUrl: './chat-app.component.html',
+  styleUrls: ['./chat-app.component.scss'],
+})
+export class ChatAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
